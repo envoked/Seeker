1b1d4ddb8d6fd5226a4946771c41df72f0c1b62d
Jeremy Lach <lach.jeremy@gmail.com>
2010-12-07 14:44:12 -0500

    fixed some stuff

--- clue/userprofile/models.py
+++ clue/userprofile/models.py
@@ -33,7 +33,7 @@ DEFAULT_AVATAR_SIZE = getattr(settings, 'DEFAULT_AVATAR_SIZE', 96)
 if DEFAULT_AVATAR_SIZE not in AVATAR_SIZES:
     DEFAULT_AVATAR_SIZE = AVATAR_SIZES[0]
 MIN_AVATAR_SIZE = getattr(settings, 'MIN_AVATAR_SIZE', DEFAULT_AVATAR_SIZE)
-DEFAULT_AVATAR = getattr(settings, 'DEFAULT_AVATAR', os.path.join(settings.MEDIA_ROOT, "userprofile", "generic.jpg"))
+DEFAULT_AVATAR = getattr(settings, 'DEFAULT_AVATAR', os.path.join(settings.MEDIA_ROOT, "userprofile", "generic.png"))
 DEFAULT_AVATAR_FOR_INACTIVES_USER = getattr(settings, 'DEFAULT_AVATAR_FOR_INACTIVES_USER', False)
 # params to pass to the save method in PIL (dict with formats (JPEG, PNG, GIF...) as keys)
 # see http://www.pythonware.com/library/pil/handbook/format-jpeg.htm and format-png.htm for options
