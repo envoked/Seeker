7e97d5882d9afc572605c35807d6e0a842e84c51
Jeremy Lach <lach.jeremy@gmail.com>
2011-03-07 23:33:50 -0500

    changed alerts to jquery dialogs

--- clue/assets/js/game.js
+++ clue/assets/js/game.js
@@ -65,12 +65,14 @@ Game = {
                 
                 if (!Game._game.is_current)
                 {
-                    alert("The game is over!")
+                    show_alert("The game is over!");
                     document.location.href = '/lobby/home/';
                 }
                 if (Game._game.new_clue)
                 {
-                    alert(Game._game.new_clue.str)
+                    //alert(Game._game.new_clue.str)
+                    clue = Game._game.new_clue.str;
+                    show_alert(clue);
                 }
                 
                 Game.redraw();             
@@ -85,12 +87,12 @@ Game = {
         this.el.css('height', $(window).height()*0.75)
         this.el.css('width', this.el.height())
         
-        for (row=0; row<this._game.board_size; row++)
+        for (col=0; col<this._game.board_size; col++)
         {
             var column = []
             var tr = $('<div class="row">')
             
-            for (col=0; col<this._game.board_size; col++)
+            for (row=0; row<this._game.board_size; row++)
             {
                 var td = $('<div class="cell">').attr('x', row).attr('y', col)
                 td.append($('<div class="inner">'))
@@ -248,7 +250,7 @@ GameCell = {
         
         else if (target.hasClass('occupied'))
         {
-            alert("You must be adjacent to a player to investigate.")
+            show_alert("You must be adjacent to a player to investigate.");
         }
     },
 }
@@ -257,6 +259,12 @@ function assign_player_avatar(element, player) {
     //element.append($('<img class="tile" src="' + Game.media_url + 'img/char1.png">'))
     //alert(player.user.username)
     element.append($('<img class="tile" src="' + player.image + '" />'));
+}
+
+function show_alert(text) {
+    text = text.replace(/ /g, "_");
+    $("#content").append("<a id='open_dialog' data-rel='dialog' href='/seeker/game/show_notification/"+text+"'>open</a>");
+    $("#open_dialog").click();
 }
 
 submitGuess = function()
